f018b6c8bc8efa08838f83fe57213139ba7f789d
SKALEZ-A <94375178+SKALEZ-A@users.noreply.github.com>
2023-04-04 23:26:39 +0100

    new

---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,11 @@
+// SPDX-Liscense-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DynamicSvgNft {
+	// mint function
+	// store our svg information somewhere
+	// some logic to show X image or Y image by switching the token URI
+
+	// this is going to be a free mint
+	function mintNft() public {}
+}
